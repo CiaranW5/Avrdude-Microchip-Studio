f2543645c45a45b59a075531e739b6c4dcb44dc6
Ciaran MacNamee <ciaran.macnamee@ul.ie>
2023-08-10 15:29:17 +0100

    Create programFlash.bat
    Batch File to be invoked in Microchip Studio

---
+++ src/programFlash.bat
@@ -0,0 +1,4 @@
+mode %1 baud=12 dtr=on > nul
+mode %1 baud=12 dtr=off > nul
+
+C:\avrdude_7_0\avrdude.exe -CC:\avrdude_7_0\avrdude.conf -v -patmega4809 -cjtag2updi -P%1 -b115200 -e -D -Uflash:w:%2:i  -Ufuse2:w:0x02:m
